32c84f2f3a526a4e66b0133532552fec8cf4876c
Victor Antunes Vieira <victor.vieira@ifac.edu.br>
2022-06-06 20:17:47 -0500

    Arquivos da aulas 06/06

---
+++ scripts.js
@@ -0,0 +1,52 @@
+console.log("Hello, world, Javascript!")
+
+//variáveis
+var x = 10 //number
+//formas mais convenientes de declarar variáveis
+let y = 50
+const z = "''"
+
+y = 20
+console.log(y)
+
+if(1){
+    let y = 15
+    console.log(y)
+}
+
+console.log(y)
+
+//tipos
+
+const nome = "Victor"
+console.log(nome)
+console.log(typeof nome)
+
+const idade = 31
+console.log(idade)
+console.log(typeof idade)
+
+const peso = 72.3
+console.log(peso)
+console.log(typeof peso)
+
+//null
+const nada = null
+console.log(nada)
+console.log(typeof nada)
+
+//undefined
+let indefinido
+console.log(indefinido)
+console.log(typeof indefinido)
+
+//bool
+const vouf = true
+console.log(vouf)
+console.log(typeof vouf)
+
+//array
+const vetor = ['Java', 10, false]
+console.log(vetor[1])
+console.log(typeof vetor)
+console.log(vetor.length)
