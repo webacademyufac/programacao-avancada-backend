1680d0bd9d0d6e6699813078d46b7162d4af2da9
vrcvieira <victor.vieira@ifac.edu.br>
2022-06-21 16:55:50 -0500

    Resolução do desafio do feriadão e alterações no pdf.

--- scripts.js
+++ scripts.js
@@ -423,7 +423,7 @@ quemEhOMelhor(
     (resultado) => {console.log(resultado.nome + resultado.mensagem)},
     (erro) => {console.log(erro.mensagem01 + melhorProgramador + erro.mensagem02)}
 )
-*/
+
 // transformando de callback para promise
 
 const melhorProgramador = 'Paulo Sampaio'
@@ -450,3 +450,17 @@ quemEhOMelhor()
     .then((resultado) => { console.log(resultado.nome + resultado.mensagem) })
     .catch((erro) => { console.log(erro.mensagem01 + melhorProgramador + erro.mensagem02) })
 
+*/
+
+fetch('http://jsonplaceholder.typicode.com/users') // Objeto do JavaScript (ES6) que funciona como uma Promise e trabalha requisições e respostas HTTP.
+    .then((resp) => resp.json()) // Recebendo os dados e convertendo para um JSON.
+    .then(function(dado){ // Recebendo os dados em um Array.
+        return dado.map(function(item){ // Conseguimos varrer o array com o método map.
+            const li = document.createElement('li') // Criando um elemento li.
+            li.innerHTML = `Nome: ${item.name} | Sobrenome: ${item.username}` // Inserindo o elemento no HTML.
+            document.getElementById('nomes').appendChild(li) // Inserindo um nó do tipo li na estrutura do DOM.
+        })
+    })
+    .catch((error) => {
+        console.log('Algo não deu certo: ' + error)
+    })
