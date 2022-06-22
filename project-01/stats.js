c2aa7b9802c466aa5986f8a7811c03f2c34c2ffe
André <andrejunioracre@gmail.com>
2022-06-22 10:04:33 -0500

    Correção de ortografia

--- project-01/stats.js
+++ project-01/stats.js
@@ -1,19 +1,20 @@
-/*
-Este código utuliza o módulo OS do Node.js para verificar o status da memória da máquina
-*/
+// Este código utuliza o módulo OS do Node.js para verificar o status da memória da máquina
 
-//Importando o módulo OS
+// Importando o módulo OS
 const os = require('os')
 
-//Utilizando desstruturação para criar as variáveis que v]ao armazenar a quantidade de memória disponível e o total da memória
+/* Utilizando desstruturação para criar as variáveis que vão armazenar a quantidade de memória 
+disponível e o total da memória */
 const { freemem, totalmem } = os
 
-//O objeto stats armazena o estado da memória com relação a quantos MB estão livres, quantos existem no total e a porcentagem que está sendo usada
+/* O objeto stats armazena o estado da memória com relação a quantos MB estão livres, quantos existem
+no total e a porcentagem que está sendo usada */
 const stats = {
     free: `${parseInt(freemem() / 1024 / 1024)} MB`,
     total: `${parseInt(totalmem() / 1024 / 1024)} MB`,
     percents: `${parseInt(( parseInt(freemem() / 1024 / 1024) / parseInt(totalmem() / 1024 / 1024 )) * 100)}%`
 }
 
+// Mostrando em um formato de tabela os dados formatados
 console.log('------ MEMORY STATS ------')
 console.table(stats)
