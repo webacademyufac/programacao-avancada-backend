2e26a472aa6e3242d5134a2bfac6c23fe55e7492
Kaua <k.g.a2014@outlook.com>
2022-06-22 19:56:41 -0500

    aula 22-06-22

---
+++ project-01/logger.js
@@ -0,0 +1,31 @@
+
+/*
+Esse código utiliza o módulo events e a classe Event Emitter para determinar uma rotina de escuta para execução de uma função.Ao final, é exportada a função log, que chama o método emit
+*/
+
+//Importando o módulo events para a classe EventEmitter
+const EventEmitter = require('events')
+const fs = require('fs')
+const { dirname } = require('path')
+const path = require('path')
+
+//Criando o objeto do tipo EventEmitter
+const emitter = new EventEmitter()
+
+//Rotina para escuta da chamada do "texto" 'log.'
+emitter.on('log' ,(mensage) => { 
+    console.log(mensage)
+    fs.appendFile(path.join(__dirname , 'log.txt' ) , mensage , (err) => {
+        if(err) throw err 
+    })
+} )
+
+//emitter.emit('log' , 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
+
+//função que faz a chamada("grita") do método emitt.
+function log(mensage){
+    emitter.emit('log', mensage)
+
+}
+
+module.exports = log 
--- project-01/stats.js
+++ project-01/stats.js
@@ -8,6 +8,8 @@ This code uses the node.js OS module to check machine stats
 
 const os = require('os')
 
+const log = require('./logger')
+
 const {freemem , totalmem} = os // Destructuring to create available memory variables and total amount of machine memory
 
 //console.log(`Memoria total : ${(freemem() / 1024 / 1024)}, ${(totalmem() / 1024 / 1024)} MB`)
@@ -25,12 +27,27 @@ const percents = (free / total) * 100
 
 //method to check the amount of ram memory of the machine
 
+
+setInterval(() => { //putting all the code inside the setinterval to keep showing every 1 second
+
+//method to check the amount of ram memory of the machine
+
+const freememory = parseInt(freemem() / 1024 / 1024 )
+const totalmemory = parseInt(totalmem() / 1024 / 1024)
+const percents = parseInt((totalmemory - freememory ) / (totalmemory ) *100)
+
 const stats = {
-    free : `${parseInt(freemem() / 1024 / 1024)} MB` ,
-    total :`${parseInt(totalmem() / 1024 / 1024)} MB`, 
-    percents : `${parseInt((parseInt(freemem() / 1024 / 1024) / parseInt(totalmem() / 1024 / 1024)) * 100)} % `
+    free : `${freememory} MB` ,
+    total :`${totalmemory} MB`  ,
+    usage : `${percents} %`
 }
 
-
+//forming the data output to table
+console.clear()
 console.log("++++++++ MEMORY RAM STATS ++++++++++++")
 console.table(stats)
+
+log(`${JSON.stringify(stats)} \n`)
+
+
+} , 1000)
