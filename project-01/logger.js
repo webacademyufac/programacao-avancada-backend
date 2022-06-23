07c2fed2b04c3aa65940b831a121abde56ac1b68
André Luiz <andrejunioracre@gmail.com>
2022-06-22 20:00:32 -0500

    Aula 22/06

---
+++ project-01/logger.js
@@ -0,0 +1,30 @@
+/* 
+Esse código utiliza o módulo events e a classe Event Emitter para determinar uma rotina de escuta para execução de uma função. Ao final, é exportada a função log, que chama o método emit.
+*/
+
+// Importando o módulo events para a classe EventEmitter.
+const EventEmitter = require('events')
+
+// Importando o módulo FileSystem para tratar a escrita em um log.
+const fs = require('fs')
+
+// Importando o módulo path para tratar do caminho.
+const path = require('path')
+
+// Criadno o objeto do tipo EventEmitter.
+const emitter = new EventEmitter()
+
+// Rotina para escuta da chamada do "texto" 'log'.
+emitter.on('log', (message) => {
+    fs.appendFile(path.join(__dirname, 'log.txt'), message, (err) => {
+        if(err) throw err
+    })
+})
+
+// Essa função faz a chamada (grita) do método emit.
+function log(message){
+    emitter.emit('log', message)
+}
+
+// Exportação do método log.
+module.exports = log
--- project-01/stats.js
+++ project-01/stats.js
@@ -3,18 +3,29 @@
 // Importando o módulo OS
 const os = require('os')
 
+const log = require('./logger')
+
 /* Utilizando desstruturação para criar as variáveis que vão armazenar a quantidade de memória 
 disponível e o total da memória */
 const { freemem, totalmem } = os
 
-/* O objeto stats armazena o estado da memória com relação a quantos MB estão livres, quantos existem
-no total e a porcentagem que está sendo usada */
-const stats = {
-    free: `${parseInt(freemem() / 1024 / 1024)} MB`,
-    total: `${parseInt(totalmem() / 1024 / 1024)} MB`,
-    percents: `${parseInt(( parseInt(freemem() / 1024 / 1024) / parseInt(totalmem() / 1024 / 1024 )) * 100)}%`
-}
+// setInterval serve para fazer o Event Loop funcionar a cada 1 segundo para o código passado no argumento
+setInterval(() => {
+    /* O objeto stats armazena o estado da memória com relação a quantos MB estão livres, quantos existem
+    no total e a porcentagem que está sendo usada */
+    const freememory = parseInt(freemem() / 1024 / 1024)
+    const totalmemory = parseInt(totalmem() / 1024 / 1024)
+    const percents = parseInt((totalmemory - freememory) / (totalmemory) * 100)
 
-// Mostrando em um formato de tabela os dados formatados
-console.log('------ MEMORY STATS ------')
-console.table(stats)
+    const stats = {
+        free: `${freememory} MB`,
+        total: `${totalmemory} MB`,
+        usage: `${percents}%`
+    }
+
+    // Mostrando em um formato de tabela os dados formatados
+    console.clear()
+    console.log('------ MEMORY STATS ------')
+    console.table(stats)
+    log(JSON.stringify(stats)+'\n')
+}, 1000);
