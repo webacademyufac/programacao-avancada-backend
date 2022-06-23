c4a084435fe32ed24bb08017b9e93cd650190e23
cleirfilho <cleirfilho@hotmail.com.br>
2022-06-22 19:56:12 -0500

    aula 2 node

--- project-01/stats.js
+++ project-01/stats.js
@@ -4,6 +4,8 @@ Eete código utuliza o módulo OS do Node.js para verificar o status da memória
 
 // Importando o modulo OS 
 const OS = require('os');
+const log = require('./logger.js');
+
 
 // utilizando destructuring para criar as variaveis que vão armazenar a quantidade de memória disponivel e a 
 // quantidade de memória total da máquina e a porsentagem de utilização de memoria. 
@@ -13,13 +15,37 @@ const {freemem, totalmem} = OS;
  Memória Total:${parseInt(totalmem()/(1024*1024))} MB
 `); */
 
+/* const freememory =  parseInt(freemem()/(1024*1024));
+const totalmemory = parseInt(totalmem()/(1024*1024));
+const percents =  parseInt(((totalmemory-freememory)/totalmemory)*100)
 
 const stats = {
-    free: `${parseInt(freemem()/(1024*1024))} MB`,
-    total:`${parseInt(totalmem()/(1024*1024))} MB`,
-    percents:`${ parseInt((parseInt(freemem()/(1024*1024))/parseInt(totalmem()/(1024*1024)))*100)} %`
+    free:freememory+'MB',
+    total:totalmemory+'MB',
+    usage:percents+'%'
 }
 
 console.log("--------- MEMORY STATS ----------------")
-console.log(stats);
+console.table(stats) */;
+
+
+// tornando o codigo assincrino 
+
+setInterval(() => {
+
+    const freememory = parseInt(freemem() / (1024 * 1024));
+    const totalmemory = parseInt(totalmem() / (1024 * 1024));
+    const percents = parseInt(((totalmemory - freememory) / totalmemory) * 100)
+
+    const stats = {
+        free: freememory + 'MB',
+        total: totalmemory + 'MB',
+        usage: percents + '%'
+    }
+    console.clear()
+    console.log("--------- MEMORY STATS ----------------")
+    console.table(stats)
+    log(JSON.stringify(stats))
+
+}, 1000)
 
