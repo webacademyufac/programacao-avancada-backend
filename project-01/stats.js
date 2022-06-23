078aec34ef1a2741cd1c7ea1474d7f6072d86ffe
abimaelqm <abimaelqm@hotmail.com>
2022-06-22 19:59:23 -0500

    Criando um serviço de gravação em log.txt a partir de verificação de memória da máquina

--- project-01/stats.js
+++ project-01/stats.js
@@ -8,6 +8,8 @@ verificar o status da memória da máquina
 
 const os = require('os')
 
+const log = require('./logger')
+
 // Desestruturação parar criar as variaveis que carregam o método para acesso a quantidade de memoria disponivel e memoria total da maquina
 const { freemem, totalmem } = os
 
@@ -15,17 +17,43 @@ const { freemem, totalmem } = os
 // Memória total: ${parseInt((totalmem()/1024) / 1024)}mb`)
 
 
-// const free = parseInt((freemem() /1024) / 1024)
-// const total = parseInt((totalmem()/1024) / 1024)
-// const percents = (free/total)*100
+// const freememory = parseInt((freemem() /1024) / 1024 )
+// const totalmemory = parseInt((totalmem()/1024) / 1024 )
+// const percents = (freememory/totalmemory)*100
+// const usagePercents = ((totalmemory - freememory)/totalmemory)*100
 
-// Objeto stats que armazenao estado da memória com relação a quantos gb estão livres, qual o total e a porcentagem usada
-const stats = {
-    free: `${parseInt((freemem() /1024) / 1024 / 1024)}gb`,
-    total: `${parseInt((totalmem()/1024) / 1024 / 1024)}gb`,
-    percents: `${(parseInt((freemem() /1024) / 1024 / 1024)/(parseInt((totalmem()/1024) / 1024 / 1024)))*100}%`
-}
-console.log(stats)
+// // Objeto stats que armazenao estado da memória com relação a quantos gb estão livres, qual o total e a porcentagem usada
+// const stats = {
+//     free: `${freememory}mb`,
+//     total: `${totalmemory}mb`,
+//     usage: `${parseInt(usagePercents)}%`
+//     // usage: `${((totalmemory - freememory)/totalmemory)*100}%`
+// }
+// // console.log(stats)
 
-// Formatando a saída dos dados da memória para uma tabela.
-console.table(stats)
+// // Formatando a saída dos dados da memória para uma tabela.
+// console.log('++++++++++ MEMORY STATS ++++++++++')
+// // console.table(stats)
+
+// log(stats)
+
+
+//  setInterval está fazendo o Event loop funcionar de 1 em 1 segundo para o código que é passado no primeiro argumento
+setInterval(()=>{
+    const freememory = parseInt((freemem() /1024) / 1024 )
+    const totalmemory = parseInt((totalmem()/1024) / 1024 )
+    const percents = (freememory/totalmemory)*100
+
+    // Objeto stats que armazenao estado da memória com relação a quantos gb estão livres, qual o total e a porcentagem usada
+    const stats = {
+        free: `${freememory} MB`,
+        total: `${totalmemory} MB`,
+        usage: `${((totalmemory - freememory)/totalmemory)*100}%`
+    }
+    
+    // Formatando a saída dos dados da memória para uma tabela.
+    console.log('++++++++++ MEMORY STATS ++++++++++')
+    console.table(stats)
+    const statsLog = JSON.stringify(stats)
+    log(`${statsLog}\n`)
+}, 1000)
