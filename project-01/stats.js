7c33fec13b97d2c1473043a5f5539033cb956cb2
Victor Antunes Vieira <victor.vieira@ifac.edu.br>
2022-06-23 20:03:30 -0500

    Alterações da aula 23 de junho.

--- project-01/stats.js
+++ project-01/stats.js
@@ -2,9 +2,9 @@
 Este código utiliza o módulo OS do Node.js para verificar o status da memória da máquina.
 */
 
-// Importando o módulo OS do Node.js.
+// Importando o módulos OS do Node.js.
 const os = require('os')
-
+// Importando a função log do documento logger.js.
 const log = require('./logger')
 
 // Utilizando desestruturação para criar as variáveis que carregam os métodos para acesso à qtd de memória disponível e à quantidade total de memória da máquina.
@@ -27,6 +27,8 @@ setInterval(() => {
     console.clear()
     console.log('++++++ MEMORY STATS ++++++')
     console.table(stats)
+    
+    // Chamada da função log, que inicia o EventEmitter.
     log(`${JSON.stringify(stats)}\n`)
 
 }, 1000)
