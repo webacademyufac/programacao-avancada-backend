5c6df711ecb214b1f210f3a81f1c69c6c0ea446f
abimaelqm <abimaelqm@hotmail.com>
2022-06-27 20:07:06 -0500

    Finalizado a criação da lista

---
+++ project-02/public/script.js
@@ -0,0 +1,67 @@
+const ul = document.querySelector('ul')
+const input = document.querySelector('#input')
+const form = document.querySelector('#form')
+
+function addElement({ name, url }) {
+
+    const li = document.createElement('li')
+    li.innerHTML = `<a target="_blank" href="${url}">${name} </a> <button onClick="removeElement(this)" class="remove">X</button>`
+    li.classList.add("li-listener")
+    // li = `<li class="li-listener" id=""> <a href="${url}">${name}: </a> <button class="btn btn-danger" id="exclui-fav">X</button></li>`
+    ul.appendChild(li)
+    // ul.innerHTML = li
+
+}
+
+function removeElement(element) {
+    // console.log(element.parentNode)
+    element.parentNode.remove()
+
+}
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    let { value } = input
+
+    if (!value) {
+        input.style['border'] = '3px solid #f55945'
+        alert('Preencha o campo!')
+    }
+
+    const [name, url] = value.split(',')
+
+    if (!url)
+        return alert('O texto não está formatado da maneira correta.')
+
+    if (/!^http/.test(url))
+        return alert('Digite a url da maneira correta.')
+
+    console.log("add")
+    addElement({ name, url })
+    input.value = ''
+    input.style['border'] = '2px inset rgb(133, 133, 133)'
+
+})
+
+
+// form.addEventListener('submit', (event) => {
+//     event.preventDefault();
+
+//     let { value } = input
+
+//     if (!value)
+//         return alert('Preencha o campo!')
+
+//     const [name, url] = value.split(',')
+
+//     if (!url)
+//         return alert('O texto não está formatado da maneira correta.')
+
+//     if (!/^http/.test(url))
+//         return alert('Digite a url da maneira correta.')
+
+//     addElement({ name, url })
+
+//     input.value = ''
+// })
--- project-02/server.js
+++ project-02/server.js
@@ -3,17 +3,31 @@ const fs = require('fs')
 const path = require('path')
 
 
-http.createServer((req, res)=>{
-    
-    if(req.url === '/')
-        fs.readFile(
-            path.join(__dirname,'public', 'index.html'), 
-            (err, content)=>{
-                if (err) throw err
-                
-                res.end(content)
-            }
-        )
+http.createServer((req, res) => {
+
+    const file = req.url === '/' ? 'index.html' : req.url
+
+    console.log(file)
+    // res.end("Cheguei aqui")
+
+    const filePath =  path.join(__dirname, 'public', file)
+
+    const extname = path.extname(filePath)
+
+    const allowedFileTypes = ['.html','.css', '.js']
+
+    const allowed = allowedFileTypes.find(item => item == extname)
+
+    if(!allowed) return
+
+    fs.readFile(
+       filePath,
+        (err, content) => {
+            if (err) throw err
+
+            res.end(content)
+        }
+    )
 
 }).listen(5000, () => console.log("Server is running..."))
 
