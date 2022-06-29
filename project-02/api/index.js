2595f1eacea9c68f6a38afa7d9c0e19bd86d5f6f
Kaua <k.g.a2014@outlook.com>
2022-06-28 19:42:41 -0500

    aula 28-06-22

---
+++ project-02/api/index.js
@@ -0,0 +1,18 @@
+const http = require('http')
+const data = require('./urls.json')
+const URL = require('url')
+
+
+http.createServer((req, res) => {
+    const{ name, url , del} = URL.parse(req.url, true).query
+
+    if(!name || !url )
+        return res.end('show')
+    if(del)
+        return res.end('delete')
+    
+    return res.end('create')
+
+
+}).listen(3000, () => console.log('API  is running. '))
+
