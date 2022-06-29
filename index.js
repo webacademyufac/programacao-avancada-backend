879851f2aed47cf26d1591e94906d32093b34374
Felipe Bezerra Lima <71532053+Bezerha@users.noreply.github.com>
2022-06-28 19:51:44 -0500

    Início API

---
+++ index.js
@@ -0,0 +1,22 @@
+const http = require('http')
+const data = require('./urls.json')
+const URL = require('url')
+
+http.createServer((req, res) => {
+
+    //res.end(JSON.stringify(data))
+
+    const {name, url, del} = (URL.parse(req.url,true).query)
+
+    if(!name || !url)
+
+        return res.end('show')
+
+    if(del)
+
+        return res.end('delete')
+
+
+    return res.end('create')
+
+}).listen(3000, () => { console.log('API is running.') })
