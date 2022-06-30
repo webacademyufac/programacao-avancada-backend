67dc96637a0bc786c9528bc019dd0b38f98c435e
André Luiz <andrejunioracre@gmail.com>
2022-06-29 19:29:34 -0500

    Aula 29/06

--- project-02/api/index.js
+++ project-02/api/index.js
@@ -1,17 +1,38 @@
 const http = require('http')
 const data = require('./urls.json')
 const URL = require('url')
+const fs = require('fs')
+const path = require('path')
 
+function writeFile(cb){
+    fs.writeFile(
+       path.join(__dirname,'urls.json'),
+       JSON.stringify(data, null, 2),
+       err => {
+           if (err) throw err
+           cb('Operação realizada com sucesso.')
+       })}
 http.createServer((req, res) => {    
-
+    
     const { name, url, del } = URL.parse(req.url, true).query
     //Se não tiver nenhum valor em name e url mostra a string 'show'
+    
+
+
     if(!name || !url)
-        return res.end('show')
+        return res.end(JSON.stringify(data))
     // Se del tiver valor = deletar
-    if(del)
-        return res.end('delete')
+    if(del){
+        data.urls = data.urls.filter(item => item.url != url)
+
+        console.log(data.urls)
+        return writeFile(message => res.end(message))
+    }
+    
+
+    data.urls.push({name,url})
+    return writeFile(message => res.end(message))
     //Se name e url tiverem valores retorna create
-    return res.end('create')
+    
 
 }).listen(8000, () => console.log('API is running'))
