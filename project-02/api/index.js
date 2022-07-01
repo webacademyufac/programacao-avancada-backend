06a66cbd655f055c35c5e563960f77004cc89cb4
Kaua <k.g.a2014@outlook.com>
2022-06-30 19:47:06 -0500

    Terminei a documentação do código Peka

--- project-02/api/index.js
+++ project-02/api/index.js
@@ -1,30 +1,46 @@
+/*
+Esse codigo cria uma API para servir ao frontend da aplicação que simula o favoritos do navegador. 
+*/
+
+
+// Importação dos módulos.
 const http = require('http')
-const data = require('./urls.json')
+
 const URL = require('url')
 const fs = require('fs');
 const path = require('path');
 
+//Importação do documento urls.json, o conteúdo do documento vai para o objeto data. 
+const data = require('./urls.json')
+
+//Sevidor roda na porta 3000
 http.createServer((req, res) => {
 
     
     res.writeHead(200 , {"Access-Control-Allow-Origin": "*"});
 
-    //destruturação da query string da URL para obter os valores do name url e para deletar
+
+   //Destruturação da query string da URL para obter os valores associados as chaves : name url e del(para deletar, que no caso é índice)
    const { name,url,del } = URL.parse(req.url,true).query
-    //mostrar o conteudo do Json
+
+
+   //Mostrar o conteúdo do Json
    if(!name || !url) 
       return res.end(JSON.stringify(data));
 
+   //Deleta conforme o índice informado 
    if(del) {
       data.urls = data.urls.filter(item => item.url != url);
       return writeFile(message => res.end(message));
    }
 
+   //Insere no json o nome e url informados 
    data.urls.push({name, url}); 
    return writeFile(message => res.end(message));
    
-}).listen(3000, () => console.log('API is running'))
+}).listen(3000, () => console.log('API is running, pao')) // configuração da porta que API está rodando
 
+//função para escrever no documento JSON. Tanto deletando como criando 
 function writeFile(cb) {
    fs.writeFile(
       path.join(__dirname, 'urls.json'),
@@ -37,4 +53,3 @@ function writeFile(cb) {
 
 
 
-//fetch('http://localhost:3000/').then(data => data.json()).then(data => console.log(data))
