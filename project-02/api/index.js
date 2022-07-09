28baab6a2700513adc5386160d83f5821d1f6fe1
crCoelhos <cristophersaar@gmail.com>
2022-07-08 22:15:11 -0500

    documentação final do project-02

--- project-02/api/index.js
+++ project-02/api/index.js
@@ -2,44 +2,26 @@
     Modulo de estudo da criaçao e consumo de api, onde há  url e nome relacionados e que devem
     ser vinculados e inseridos em um banco de dados não relacional, consultado e modificado(excluido)
 */
-
-// importaçoes dos modulos utilizadps
+// importaçoes dos modulos utilizados
 const http = require('http')
-const data = require('./urls.json')
 const URL = require('url')
 const fs = require('fs')
 const path = require('path')
 
+// importação do json
+const data = require('./urls.json')
+
 // criação do server na porta 3000
 http.createServer((req, res) => {
-    // adequaçao para não cair no CORS 
-    const header = {
-        'Access-Control-Allow-Origin': '*', //permitir todas as origens
-        'Access-Control-Allow-Methods': 'DELETE, POST, GET', //metodos de requisição utilizados na aplicação
-        'Access-Control-Max-Age': 2592000, //em segundos
-    };
-    if (req.method === "POST")
-    res.writeHead(
-        200, {
-            "Access-Control-Allow-Origin": "*"
-        }
-    )
-    // if (req.method === "DELETE")
-    // res.writeHead(
-    //     200, {
-    //         "Access-Control-Allow-Origin": "*"
-    //     }
-    // )
+    // adequaçao do cabeçalho para permissão de CORS 
+    res.setHeader('Access-Control-Allow-Origin', '*')//permitir todas as origens
+    res.setHeader('Access-Control-Allow-Methods', 'POST,GET,DELETE,UPDATE')//metodos de requisição utilizados na aplicação
+    res.setHeader('Access-Control-Max-Age', '256100')//em segundos
 
+    // Desestruturação da query para criação de variaveis
+    const { name, url, del } = URL.parse(req.url, true).query
 
-    const {
-        name,
-        url,
-        del
-    } = URL.parse(req.url, true).query //formado da query a ser utilizada no url no modelo:
-    // http://localhost:3000/urls?name=Google&url=https://www.google.com&del=1
-
-    // escrever o json
+    // Função para escrever e reescrever o documento
     function writeFile(cb) {
         fs.writeFile(
             path.join(__dirname, 'urls.json'),
@@ -50,23 +32,19 @@ http.createServer((req, res) => {
             }
         )
     }
-
-    //condicionais da query para realizar a requisição
-    if (!name || !url) { //caso algo esteja vazio
+    
+    // exibe o documento
+    if (!name || !url) {
         return res.end(JSON.stringify(data))
     }
-    if (del) { //caso &del=1, para deletar 
+    // deleta e reescreve o documento
+    if (del) {
         data.urls = data.urls.filter(item => item.url != url)
         writeFile(message => res.end(message))
         return data.urls
     }
-    // caso haja name e url para incluir
-    data.urls.push({
-        name,
-        url
-    })
+    data.urls.push({name, url})
     writeFile(message => res.end(message))
-    return data.urls //exibe o json
+    return data.urls
 
-}).listen(3000, () => console.log("server's runing"))
-// servidor rodando na porta 3000
+}).listen(3000, () => console.log('Api is running(port 3000)'))
--- project-02/public/script.js
+++ project-02/public/script.js
@@ -3,54 +3,48 @@ const input = document.querySelector('#input')
 const form = document.querySelector('#form')
 
 // função para coleta de dados do backend 
-async function load() {
+window.addEventListener('load', e =>{
+    
+    fetch(`http://localhost:3000/`)
+        .then(content => content.json())
+        // console.log(res.urls)
 
-    const res = await fetch('http://localhost:3000/')
-        .then(data => data.json())
-    // console.log(res.urls)
-    res.urls.map(({
-        name,
-        url
-    }) => addElement({
-        name,
-        url
-    }))
+        .then(data => data.urls.map(({name, url}) => showElement({name, url})))
+})
 
-}
-load()
-
-// função cria o elemento <li> com o conteudo composto de um nome dentro de um <a> com o href dele sendo a url como valor 
-function addElement({
-    name,
-    url
-}) {
+// função cria o elemento <li> com o conteudo composto de um nome dentro de um <a> com o href dele sendo a url como valor, fazendo uso de desestruturação de um object como parametro
+function showElement({ name, url }) {
     const li = document.createElement('li')
-    li.innerHTML = `<a target="_blank" href="${url}">${name} </a> <button onClick="removeElement(this)" class="remove">X</button>`
+    li.innerHTML = `<a target="_blank" href="${url}">${name}</a> <button onClick="removeElement(this)" class="remove">X</button>`
     li.classList.add("li-listener")
     ul.appendChild(li)
-
 }
 
-// função para remover pai do elemento clicado com todo seu conteudo
+// Função que tem como argumento um object desestruturado com os chaves name e ur, que manda uma requisição com a url tratada da forma como se espera para adicionar o elemento no arquivo .json
+function addElement({name, url}){
+    const urlTratada = url.replace(" ","")//parse de espaço na url
+    fetch(`http://localhost:3000/?name=${name}&url=${urlTratada}`)
+}
+
 function removeElement(element) {
-    // console.log(element.parentNode)
-
-    // element.parentNode.style['background'] = 'red'
-    // element.parentNode.style['visibility'] = 'hidden'
-    element.parentNode.remove()
+    // faz uso do conteúdo do elemento atributo href de a, e joga pro fetch 
+    const name = element.parentNode.children[0].textContent
+    const href = element.parentNode.children[0].href
+    const url = href.substring(0, href.length - 1);
+    fetch(`http://localhost:3000/?name=${name}&url=${url}&del=1`)
 
 }
 
-// "ouvinte" do botão enviar 
+// chama a função através de submit
 form.addEventListener('submit', (event) => {
     event.preventDefault();
 
-    let {
-        value
-    } = input
+    // recolhe valor do input
+    let { value } = input
 
+    //sinalização na caixa do inbox da necessidade de corrigir os parametros
     if (!value) {
-        input.style['border'] = '3px solid #f55945'//sinalização na caixa do inbox da necessidade de corrigir os parametros
+        input.style['border'] = '3px solid #f55945'
         alert('Preencha o campo!')
     }
 
@@ -59,26 +53,12 @@ form.addEventListener('submit', (event) => {
     if (!url)
         return alert('O texto não está formatado da maneira correta.')
 
+    // sinalização na caixa do inbox da necessidade de corrigir os parametros
     if (/!^http/.test(url))
         return alert('Digite a url da maneira correta.')
-
-    // console.log("add")
     // criação e inserção do elemento na pagina através da chamada de função responsavel
-    addElement({
-        name,
-        url
-    })
-
-    fetch('http://localhost:3000/', {
-        method: "POST", // or "PUT" with the url changed to, e.g "https://reqres.in/api/users/2"
-        headers: {
-            'Content-type': 'application/json'
-        },
-        body: JSON.stringify({
-            name: "Captain Anonymous"
-        })
-    });
-    input.value = ''
-    input.style['border'] = '2px inset rgb(133, 133, 133)' //remoção da sinalização na caixa do inbox da necessidade de corrigir os parametros
+    addElement({ name, url })
+    input.value = ''//remoção da sinalização na caixa do inbox da necessidade de corrigir os parametros
+    input.style['border'] = '2px inset rgb(133, 133, 133)'
 
 })
