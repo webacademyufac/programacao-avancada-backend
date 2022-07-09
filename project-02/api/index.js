9856a8e6e66d9a923a40f9777469acfc3ad85fdb
André Luiz <andrejunioracre@gmail.com>
2022-07-08 19:02:57 -0500

    API Finalizada

--- project-02/api/index.js
+++ project-02/api/index.js
@@ -5,73 +5,73 @@ const fs = require('fs')
 const path = require('path')
 
 // Escrevendo no arquivo através do writeFile, passando o caminho do arquivo JSON.
-// function writeFile(cb) {
-//     fs.writeFile(
-//         path.join(__dirname, 'urls.json'),
-//         JSON.stringify(data, null, 2),
-//         err => {
-//             if (err) throw err
-//             cb('Operação realizada com sucesso.')
-//         }
-//     )
-// }
+function writeFile(cb) {
+    fs.writeFile(
+        path.join(__dirname, 'urls.json'),
+        JSON.stringify(data, null, 2),
+        err => {
+            if (err) throw err
+            cb('Operação realizada com sucesso.')
+        }
+    )
+}
 
-// http.createServer((req, res) => {
+http.createServer((req, res) => {
 
-//     // Controle da política CORS.
-//     // res.writeHead(
-//     //     200,
-//     //     {"Access-Control-Allow-Origin":"*"}
-//     // )
+    // Controle da política CORS.
+    // res.writeHead(
+    //     200,
+    //     {"Access-Control-Allow-Origin":"*"}
+    // )
 
-//     res.setHeader('Access-Control-Allow-Origin', '*')
-//     res.setHeader('Access-Control-Allow-Methods', 'POST, GET, DELETE, UPDATE')
-//     res.setHeader('Access-Control-Max-Age', 2592000)
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, DELETE, UPDATE')
+    res.setHeader('Access-Control-Max-Age', 2592000)
 
-//     const { name, url, del } = URL.parse(req.url, true).query
+    const { name, url, del } = URL.parse(req.url, true).query
 
-//     //Se não tiver nenhum valor em name e url mostra os dados do arquivo JSON.
-//     if (!name || !url)
-//         return res.end(JSON.stringify(data))
+    //Se não tiver nenhum valor em name e url mostra os dados do arquivo JSON.
+    if (!name || !url)
+        return res.end(JSON.stringify(data))
         
-//     // Se del tiver valor = deletar
-//     if (del) {
-//         data.urls = data.urls.filter(item => item.url != url)
-//         return writeFile(message => res.end(message))
-//     }
+    // Se del tiver valor = deletar
+    if (del) {
+        data.urls = data.urls.filter(item => item.url != url)
+        return writeFile(message => res.end(message))
+    }
 
-//     //Se name e url tiverem valores retorna create
-//     data.urls.push({ name, url })
-//     return writeFile(message => res.end(message))
+    //Se name e url tiverem valores retorna create
+    data.urls.push({ name, url })
+    return writeFile(message => res.end(message))
 
-// }).listen(8000, () => console.log('API is running'))
+}).listen(8000, () => console.log('API is running'))
 
 // -------------- API COM MONGODB --------------
 
-const express = require('express')
-const mongoose = require('mongoose')
-const app = express()
-const cors = require('cors')
-const linkRoutes = require('./routes/linkRoutes')
+// const express = require('express')
+// const mongoose = require('mongoose')
+// const app = express()
+// const cors = require('cors')
+// const linkRoutes = require('./routes/linkRoutes')
 
-app.use(express.json())
+// app.use(express.json())
 
-app.use(
-    express.urlencoded({
-        extended: true,
-    }),
-    cors()
-)
+// app.use(
+//     express.urlencoded({
+//         extended: true,
+//     }),
+//     cors()
+// )
 
-app.use('/', linkRoutes)
+// app.use('/', linkRoutes)
 
 
-const DB_USER = 'root'
-const DB_PASSWORD = encodeURIComponent('root')
+// const DB_USER = 'root'
+// const DB_PASSWORD = encodeURIComponent('root')
 
-mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@programacao-avancada.9ctapaf.mongodb.net/test?retryWrites=true&w=majority`)
-    .then(() => {
-        console.log('Conectado ao MongoDB!')
-        app.listen(8000)
-    })
-    .catch((err) => console.log(err))
+// mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@programacao-avancada.9ctapaf.mongodb.net/test?retryWrites=true&w=majority`)
+//     .then(() => {
+//         console.log('Conectado ao MongoDB!')
+//         app.listen(8000)
+//     })
+//     .catch((err) => console.log(err))
